533f41950031bac9fa5d173c7599359179b548d2
KamariRoss <kamarimobile@gmail.com>
2020-03-21 13:21:57 -0400

    start porject

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
-
-const App = () => {
-  return <HelloWorld />
+// This is your first component
+// This is your entire page
+// This is where the HTML goes
+class App extends React.Component {
+  render() {
+    return (
+      <article>
+        <header>
+          <h1>Color Picker</h1>
+          <p>By Kamari Ross</p>
+        </header>
+        <section>
+          <section></section>
+          <section></section>
+          <section>
+            <section></section>
+          </section>
+        </section>
+        <footer>Finish Line</footer>
+      </article>
+    )
+  }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// This i where were getting the render function
 import './index.css'
+// assumes the app is javascript
 import App from './App'
 
+// This is the file that starts the app
 ReactDOM.render(<App />, document.getElementById('root'))
